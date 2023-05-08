87a5358e06a4c1af5917ffa3171ff874df5e5023
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2023-05-07 19:35:02 -0700

    changed SITEURL to github pages url and set RELATIVE_URLS=True site seems to publish correctly

--- publishconf.py
+++ publishconf.py
@@ -7,8 +7,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://ocerte.org'
-RELATIVE_URLS = False
+SITEURL = 'https://ocerte.github.io/ocerte-website'
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
